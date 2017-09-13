271e9ceb0e458adff7c5ed24625334be95c4149d
Mikaeil Orfanian <mokt@outlook.com>
2017-09-13 17:15:37 +0200

    Prepare deploy to pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@
 from setuptools import setup
 
 
-with open('README.md', 'r') as f:
-    readme = f.read()
-
-
 setup(
     name='djforge_redis_multitokens',
     version='0.0.2',
@@ -15,13 +11,12 @@ setup(
     author='Mikaeil Orfanian',
     description='Django REST Framework user auth using multiple tokens stored in Redis',
     author_email='it@toreforge.com',
-    long_description=readme,
     install_requires=['passlib', 'djangorestframework', 'django'],
     include_package_data=True,
     url='https://github.com/ToReforge/djforge-redis-multitokens/archive/0.0.2.tar.gz',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Framework :: Django REST Framework',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
